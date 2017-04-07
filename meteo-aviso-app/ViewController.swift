936475c27a2f29d4dcef7499c4e0d5c91ed0e039
egaillera <egaillera@gmail.com>
2017-04-07 17:03:44 +0200

    Added key in info.plist to allow App Transport Security enabled

--- meteo-aviso-app/ViewController.swift
+++ meteo-aviso-app/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "http://meteoaviso.cloudapp.net:9090/stations")
+        
+        let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+            let myData = String(data: data!, encoding: String.Encoding.utf8)
+            print(myData)
+        }
+        
+        task.resume()
+    
+      
     }
 
     override func didReceiveMemoryWarning() {
