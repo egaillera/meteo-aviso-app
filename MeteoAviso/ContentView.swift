123a030623d7ef12b0963b2f001cc5bde9cdd681
Enrique Garcia Illera <egaillera@gmail.com>
2020-08-24 16:06:00 +0200

    Initial Commit

---
+++ MeteoAviso/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeteoAviso
+//
+//  Created by Enrique Garcia Illera on 24/08/2020.
+//  Copyright © 2020 Enrique Garcia Illera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
