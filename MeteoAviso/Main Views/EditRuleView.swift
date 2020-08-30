383f18664fad7979169fe75a7d498ae99e69c66d
Enrique Garcia Illera <egaillera@gmail.com>
2020-08-30 12:33:28 +0200

    [FEAT] Added button to save rules in the EditRule view

--- MeteoAviso/Main Views/EditRuleView.swift
+++ MeteoAviso/Main Views/EditRuleView.swift
@@ -20,20 +20,29 @@ struct EditRuleView: View {
     var body: some View {
         print("File: \(#file), Function: \(#function), line: \(#line)")
         return VStack {
-            Text("Editar reglas para \(stationName)")
-            Text("Codigo estación: \(stationCode)")
+            VStack {
+                Text("Editar reglas para \(stationName)").padding()
+                Text("Codigo estación: \(stationCode)").padding()
+            }.padding()
+            Spacer()
             HStack {
                 Text("Umbral de lluvia: \(maxRainTh)")
                 Slider(value:$maxRainTh,in:0...200,step: 1.0)
-            }
+            }.padding()
             HStack {
                 Text("Umbral de temperatura maxima: \(maxTempTh)")
                 Slider(value:$maxTempTh,in:-50...50,step: 1.0)
-            }
+            }.padding()
             HStack {
                 Text("Umbral de tempratura minima: \(minTempTh)")
                 Slider(value:$minTempTh,in:-50...50,step: 1.0)
+            }.padding()
+            Spacer()
+            HStack {
+                Button(action: {}) {Text("Guardar")}.padding()
+                Button(action: {}) {Text("Cancelar")}.padding()
             }
+            Spacer()
         }
     }
 }
